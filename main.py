b1f759c8ce0aa5744af0c52a2eeef90648063bc3
dyavadi8769 <sdyavadi@tamu.edu>
2025-03-03 19:28:21 -0600

    Util functions were implemented

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+
+from util import get_parking_spots_bboxes, empty_or_not
+
+
+def calc_diff(im1, im2):
+    return np.abs(np.mean(im1) - np.mean(im2))
+
+
+mask = './mask_1920_1080.png'
+video_path = './data/parking_1920_1080_loop.mp4'
+
+
+mask = cv2.imread(mask, 0)
+
+cap = cv2.VideoCapture(video_path)
+
+connected_components = cv2.connectedComponentsWithStats(mask, 4, cv2.CV_32S)
+
+spots = get_parking_spots_bboxes(connected_components)
+
+spots_status = [None for j in spots]
+
+diffs = [None for j in spots]
--- util.py
+++ util.py
@@ -1,16 +1,39 @@
+import pickle
+
+from skimage.transform import resize
+import numpy as np
 import cv2
 
-video_path = './data/parking_crop_loop.mp4'
 
-cap = cv2.VideoCapture(video_path)
+def empty_or_not(spot_bgr):
 
-ret=True
-while ret:
-    ret, frame = cap.read()
-    
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+    flat_data = []
 
-cap.release()
-cv2.destroyAllWindows()
+    img_resized = resize(spot_bgr, (15, 15, 3))
+    flat_data.append(img_resized.flatten())
+    flat_data = np.array(flat_data)
+
+    y_output = MODEL.predict(flat_data)
+
+    if y_output == 0:
+        return EMPTY
+    else:
+        return NOT_EMPTY
+
+
+def get_parking_spots_bboxes(connected_components):
+    (totalLabels, label_ids, values, centroid) = connected_components
+
+    slots = []
+    coef = 1
+    for i in range(1, totalLabels):
+
+        # Now extract the coordinate points
+        x1 = int(values[i, cv2.CC_STAT_LEFT] * coef)
+        y1 = int(values[i, cv2.CC_STAT_TOP] * coef)
+        w = int(values[i, cv2.CC_STAT_WIDTH] * coef)
+        h = int(values[i, cv2.CC_STAT_HEIGHT] * coef)
+
+        slots.append([x1, y1, w, h])
+
+    return slots
